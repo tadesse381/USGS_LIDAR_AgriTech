af3e9a225bed6e58f7dfb43e9b5a3482a6a79360
Tadesse-Kebede <85294225+tadesse381@users.noreply.github.com>
2022-06-25 09:42:41 +0300

    lidar data fetch

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -1,28 +1,27 @@
-from file_handler import FileHandler
 import os
 import sys
 import unittest
 import pandas as pd
 
 sys.path.append(os.path.abspath(os.path.join('../scripts')))
+from file_handler import FileHandler
 
 
-class TestFileHandler(unittest.TestCase):
+class TestDfHelper(unittest.TestCase):
 
-  def setUp(self) -> pd.DataFrame:
-    self.helper = FileHandler()
+    def setUp(self):
+        self.helper = FileHandler()
 
-  def test_save_csv(self):
-    df = pd.DataFrame({'col1': [1, 2, 1], 'col2': [3, 4, 3]})
-    self.helper.save_csv(df, './test.csv', False)
-    df2 = pd.read_csv('test.csv')
-    self.assertEqual(df.shape, df2.shape)
+    def test_to_csv(self):
+        df = pd.DataFrame({'col1': range(1,4), 'col2': range(3,6)})
+        self.helper.to_csv(df, './test.csv', False)
+        df2 = pd.read_csv('test.csv')
+        self.assertEqual(df.shape, df2.shape)
 
-  def test_read_csv(self):
-    df = self.helper.read_csv('test.csv')
-    df2 = pd.read_csv('test.csv')
-    self.assertEqual(df.shape, df2.shape)
-
+    def test_read_csv(self):
+        df = self.helper.read_csv('test.csv')
+        df2 = pd.read_csv('./test.csv')
+        self.assertEqual(df.shape, df2.shape)
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
